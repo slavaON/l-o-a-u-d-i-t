4b7f28119b979b6dc92e821d744dd75491317e1d
SV <97764293+slavaON@users.noreply.github.com>
2024-01-03 15:25:37 +0200

    Update constants.ts

--- scr/utilities/constants.ts
+++ scr/utilities/constants.ts
@@ -9,7 +9,6 @@ export const PROVIDERS = {
     era: await getRPC('https://zksync.meowrpc.com'),
     polygon: await getRPC('https://rpc.ankr.com/polygon'),
     celo: await getRPC('https://rpc.ankr.com/celo'),
-    moonbeam: await getRPC('https://rpc.ankr.com/moonbeam'),
     moonriver: await getRPC('https://moonriver.public.blastapi.io'),
     conflux: await getRPC('https://evm.confluxrpc.com'),
     gnosis: await getRPC('https://rpc.ankr.com/gnosis'),
@@ -33,13 +32,6 @@ export const TICKERS: Ticker[] =
             fee: 0.0008,
             amount: exchange.amounts.celo,
 
-        },
-        {
-            name: 'moonbeam',
-            network: "Moonbeam",
-            symbol: "GLMR",
-            fee: 0.01,
-            amount: exchange.amounts.moonbeam,
         },
         {
             name: 'moonriver',
@@ -77,7 +69,6 @@ const CONTRACT_ADDRESSES = {
     polygon: {
         merkly: "0x0E1f20075C90Ab31FC2Dd91E536e6990262CF76d",
         l2pass: "0x222228060E7Efbb1D78BB5D454581910e3922222",
-        zerius: "0x2ef766b59e4603250265EcC468cF38a6a00b84b3", //@audit - unknown  address
         l2telegraph: "0x523d5581A0bb8BB2Bc9f23B5202894E31124eA3e",
     },
     celo: {
@@ -89,10 +80,6 @@ const CONTRACT_ADDRESSES = {
         merkly: "0x556F119C7433b2232294FB3De267747745A1dAb4",
         l2pass: "0x222228060E7Efbb1D78BB5D454581910e3922222",
         l2telegraph: "0xE266EedB13A69AF15c1756a241021905B1827F6A",
-    },
-    moonbeam: {
-        merkly: "0x671861008497782F7108D908D4dF18eBf9598b82",
-        l2pass: "0x222228060E7Efbb1D78BB5D454581910e3922222",
     },
     moonriver: {
         merkly: "0xd379c3D0930d70022B3C6EBA8217e4B990705540",
@@ -106,15 +93,6 @@ const CONTRACT_ADDRESSES = {
         merkly: "0x79DB0f1A83f8e743550EeB5DD5B0B83334F2F083",
         l2pass: "0x222228060E7Efbb1D78BB5D454581910e3922222",
     },
-}
-
-// @audit - unknown addresses 
-export const ZERIUS_REFUEL_CONTRACTS: any = {
-    145: '0x1fe2c567169d39CCc5299727FfAC96362b2Ab90E',
-    116: '0x5B209E7c81DEaad0ffb8b76b696dBb4633A318CD',
-    153: '0xB47D82aA70f839dC27a34573f135eD6dE6CED9A5',
-    125: '0xFF21d5a3a8e3E8BA2576e967888Deea583ff02f8',
-    126: '0xb0bea3bB2d6EDDD2014952ABd744660bAeF9747d',
 }
 
 export const CHAINS: Record<string, Chain> = {
@@ -172,9 +150,7 @@ export const CHAINS: Record<string, Chain> = {
         },
         // @audit - what are spenders addresses?
         spenders: [
-            "0x000000000022d473030f116ddee9f6b43ac78ba3", "0xaB235da7f52d35fb4551AfBa11BFB56e18774A65",
-            "0xe3d8bd6aed4f159bc8000a9cd47cffdb95f96121", "0x471EcE3750Da237f93B8E339c536989b8978a438",
-
+            "0x000000000022d473030f116ddee9f6b43ac78ba3"
         ],
         tokens: [
             "0x617f3112bf5397D0467D315cC709EF968D9ba546", "0xef4229c8c3250C675F21BCefa42f58EfbfF6002a",
@@ -182,31 +158,6 @@ export const CHAINS: Record<string, Chain> = {
             "0x471EcE3750Da237f93B8E339c536989b8978a438", "0x29dFce9c22003A4999930382Fd00f9Fd6133Acd1"
         ],
 
-    },
-    moonbeam: {
-        minBalance: 0.3,
-        names: ["merkly", "l2pass"],
-        protocols: {
-            merkly: {
-                contract: await getContract(CONTRACT_ADDRESSES.moonbeam.merkly, ABI.merkly, PROVIDERS.moonbeam),
-                dstChains: [115, 125, 116],
-            },
-            l2pass: {
-                contract: await getContract(CONTRACT_ADDRESSES.moonbeam.l2pass, ABI.l2pass, PROVIDERS.moonbeam),
-                dstChains: [145, 125, 116],
-            },
-        },
-        // @audit - what are spenders addresses?
-        spenders: [
-            "0x843D0AAD40295f2198ef528ad747CDF6AB9000e4", "0xd95fe880d7717f7f20981FE6e41A2315f3EFeAb5",
-            "0x603eF396029b5e89f9420b4192814aEC0664ADAb", "0xd3B02Ff30c218c7f7756BA14bcA075Bf7C2C951e",
-        ],
-        tokens: [
-            "0xeFAeeE334F0Fd1712f9a8cc375f427D9Cdd40d73", "0x931715FEE2d06333043d11F658C8CE934aC61D0c",
-            "0x4792C1EcB969B036eb51330c63bD27899A13D84e", "0x3405A1bd46B85c5C029483FbECf2F3E611026e45",
-            "0x818ec0A7Fe18Ff94269904fCED6AE3DaE6d6dC0b", // @audit - This token is bridged from its native chain using Multichain. But Metamsk mark this website as:
-            // https://metamask.github.io/phishing-warning/v3.0.0/#hostname=multichain.org&href=https%3A%2F%2Fmultichain.org%2F
-        ],
     },
     moonriver: {
         minBalance: 0.01,
@@ -227,34 +178,14 @@ export const CHAINS: Record<string, Chain> = {
         },
         // @audit - what are spenders addresses?
         spenders: [
-            "0x7af71799C40F952237eAA4D81A77C1af49125113", "0xD8FC27ec222E8d5172CD63aC453C6Dfb7467a3C7",
-            "0xFB45b575b66C99e0C8d2639aCf237807d4ea1508",
+            "0xAA30eF758139ae4a7f798112902Bf6d65612045f"
 
         ],
         tokens: [
-            "0xB44a9B6905aF7c801311e8F4E76932ee959c663C", "0xE3F5a90F9cb311505cd691a46596599aA1A0AD7D",
-            "0x80A16016cC4A2E6a2CACA8a4a498b1699fF0f844", "0x98878B06940aE243284CA214f92Bb71a2b032B8A",
+            "0x1a93b23281cc1cde4c4741353f3064709a16197d", "0xe3f5a90f9cb311505cd691a46596599aa1a0ad7d",
+            "0x6bd193ee6d2104f14f94e2ca6efefae561a4334b"
         ],
 
-    },
-    conflux: {
-        minBalance: 0.4,
-        names: ["merkly"],
-        protocols: {
-            merkly: {
-                contract: await getContract(CONTRACT_ADDRESSES.conflux.merkly, ABI.merkly, PROVIDERS.conflux),
-                dstChains: [125],
-            },
-        },
-        // @audit - what are spenders addresses?
-        spenders: [
-            "0x62b0873055Bf896DD869e172119871ac24aEA305",
-        ],
-        tokens: [
-            "0xfe97e85d13abd9c1c33384e796f10b73905637ce", "0xa47f43de2f9623acb395ca4905746496d2014d57",
-            "0x6963efed0ab40f6c3d7bda44a05dcf1437c44372", "0x94bd7a37d2ce24cc597e158facaa8d601083ffec",
-            "0x889138644274a7dc602f25a7e7d53ff40e6d0091", "0x14b2d3bc65e74dae1030eafd8ac30c533c976a9b",
-        ],
     },
     gnosis: {
         minBalance: 0.0012,
@@ -276,12 +207,12 @@ export const CHAINS: Record<string, Chain> = {
         // @audit - what are spenders addresses?
         spenders: [
             "0x1111111254EEB25477B68fb85Ed929f73A960582", "0x7A4af156379f512DE147ed3b96393047226d923F",
-            "0xBA12222222228d8Ba445958a75a0704d566BF2C8", "0x6093AeBAC87d62b1A5a4cEec91204e35020E38bE",
+            "0xBA12222222228d8Ba445958a75a0704d566BF2C8"
 
         ],
         tokens: [
             "0x4ECaBa5870353805a9F068101A40E0f32ed605C6", "0xDDAfbb505ad214D7b80b1f830fcCc89B60fb7A83",
-            "0xE2e73A1c69ecF83F464EFCE6A5be353a37cA09b2", "0x7122d7661c4564b7C6Cd4878B06766489a6028A2"
+            "0xE2e73A1c69ecF83F464EFCE6A5be353a37cA09b2"
         ],
     },
     klaytn: {
@@ -298,7 +229,9 @@ export const CHAINS: Record<string, Chain> = {
             },
         },
         // @audit - what are spenders addresses?
-        spenders: ["0xe0fbB27D0E7F3a397A67a9d4864D4f4DD7cF8cB9", "0xF7BF3499Df413aC09C4BC8F7521EB4953B5f7bda"],
+        spenders: ["0xc6a2ad8cc6e4a7e08fc37cc5954be07d499e7654"
+                   
+            ],
         tokens: [
             "0x34d21b1e550d73cee41151c77f3c73359527a396", "0xcee8faf64bb97a73bb51e115aa89c17ffa8dd167",
             "0x574e9c26bda8b95d7329505b4657103710eb32ea", "0x754288077d0ff82af7a5317c7cb8c444d421d103",
